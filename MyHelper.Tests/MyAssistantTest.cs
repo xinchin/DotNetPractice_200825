bccf5403335efe5943a62ecd434ec649829edeb2
Nelson Huang <xinchin@gmail.com>
2020-08-30 15:42:46 +0800

    增加 NUnit 的測試專案及範例

---
+++ MyHelper.Tests/MyAssistantTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MyHelper.Tests
+{
+    [TestFixture]
+    public class MyAssistantTest
+    {
+        [Test]
+        public void Test_Add()
+        {
+            //Arrange
+            int expectValue = 8;
+
+            //Act
+            int actualValue = MyAssistant.Add(3, 5);
+
+            //Assert
+            Assert.AreEqual(expectValue, actualValue);
+        }
+
+    }
+}
---
+++ MyHelper/MyAssistant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelper
+{
+    public class MyAssistant
+    {
+
+        static public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
