0b6adfc2e939c49d5ce519725657023511488065
Nelson Huang <xinchin@gmail.com>
2020-09-04 03:57:55 +0800

    新增 grpc 練習範例

--- GrpcServer.Web/Services/MyEmployeeService.cs
+++ GrpcServer.Web/Services/MyEmployeeService.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grpc.Core;
 using GrpcServer.Web.Protos;
+using Microsoft.Extensions.Logging;
 
 namespace GrpcServer.Web.Services
 {
     public class MyEmployeeService :EmployeeService.EmployeeServiceBase
     {
+
+        private readonly ILogger<MyEmployeeService> _logger;
+
+        public MyEmployeeService(ILogger<MyEmployeeService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<EmployeeResponse> getByNo(GetByNoRequest request, ServerCallContext context)
+        {
+            return base.getByNo(request, context);
+        }
     }
 }
