7a6db9170284d8a0315066417175759a69427ac3
Nelson Huang <xinchin@gmail.com>
2020-09-04 03:54:41 +0800

    新增 grpc 練習範例

---
+++ GrpcServer.Web/Services/MyEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcServer.Web.Protos;
+
+namespace GrpcServer.Web.Services
+{
+    public class MyEmployeeService :EmployeeService.EmployeeServiceBase
+    {
+    }
+}
