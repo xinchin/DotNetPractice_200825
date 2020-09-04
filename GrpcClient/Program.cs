d5bc5edf7b30af9eaf13d323bd17876480f0d74f
Nelson.Huang <xinchin@gmail.com>
2020-09-04 09:45:49 +0800

    Add Project : gRPC Client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer.Web.Protos;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new EmployeeService.EmployeeServiceClient(channel);
+            var response = await client.GetByNoAsync(new GetByNoRequest
+            {
+                No = 1977
+            }); ;
+
+            Console.WriteLine($"Response messages : {response}");
+            Console.WriteLine($"Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GrpcServer.Web/Data/InMemoryData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcServer.Web.Protos;
+
+namespace GrpcServer.Web.Data
+{
+    public class InMemoryData
+    {
+        public static List<Employee> Employees = new List<Employee>
+        {
+            new Employee{
+                Id =1,
+                No=1977,
+                Firstname="Nelson",
+                Lastname="Huang",
+                Salary=1000000
+            },
+            new Employee{
+                Id =2,
+                No=1978,
+                Firstname="Xinchin",
+                Lastname="Huang",
+                Salary=1000000
+            },
+            new Employee{
+                Id =3,
+                No=1979,
+                Firstname="Nelson Huang",
+                Lastname="Huang",
+                Salary=1000000
+            },
+
+        };
+    }
+}
